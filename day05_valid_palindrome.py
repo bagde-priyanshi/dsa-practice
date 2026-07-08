5967c3bd86dfbf3bb8f7fd68bed946bf18faefe4
bagde-priyanshi <bagdepriyanshi07@gmail.com>
2026-07-08 19:07:31 +0530

    Implement Two Pointer method for palindrome check
    Added a Two Pointer method for palindrome checking using a while loop.

--- day05_valid_palindrome.py
+++ day05_valid_palindrome.py
@@ -1,3 +1,4 @@
+# Two Pointer method using in-built python string manipulation and class
 class Solution(object):
     def isPalindrome(self, s):
         """
@@ -17,3 +18,25 @@ solution = Solution()
 print(solution.isPalindrome("A man, a plan, a canal; Panama"))
 print(solution.isPalindrome("race a car"))
 print(solution.isPalindrome(""))
+
+# Two Pointer Method for Palindrome checking in a string using While Loop
+
+string = "racecar"
+string = string.lower()
+
+left = 0
+right = len(string) - 1
+is_palindrome = True
+
+while left < right:
+    if string[left] != string[right]:
+        is_palindrome = False
+        break
+    left += 1
+    right -= 1
+
+if is_palindrome:
+    print(f"{string} is a palindrome")
+else:
+    print(f"{string} is not a palindrome")
+
